3ceaefbcc2d41e04174d19c7ad0d7ab729fbe6b5
ibrahim <ibrahimrehman88@gmail.com>
2022-04-15 11:23:07 +0500

    fifth commit

--- test_param.py
+++ test_param.py
@@ -23,7 +23,7 @@ options = Options()
 options.headless = True
 #PATH = "C:\Program Files (x86)\chromedriver.exe"
 PATH = r"E:\chromedriver.exe"
-#this is commit
+#this is fifth committ
 
 chrome_options = webdriver.ChromeOptions(); 
 chrome_options.add_experimental_option("excludeSwitches", ['enable-automation']);
